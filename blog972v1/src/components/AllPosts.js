7842d3ba3756d99eab90105288ed2e85a8b931b7
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2022-03-06 16:20:29 +0100

    app routing setup completed

---
+++ blog972v1/src/components/AllPosts.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function AllPosts(){
+    return <h2>
+                All Posts Page
+            </h2>;
+}
---
+++ blog972v1/src/components/OnePost.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function OnePost(){
+    return <h2>
+                This Page will have 1 Post
+            </h2>;
+}
